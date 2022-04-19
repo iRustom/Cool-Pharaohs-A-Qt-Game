db9ff276300daf411010e8a219f8d81da2e629ef
OmarElfouly <omarelfouly@aucegypt.edu>
2022-04-20 00:06:14 +0200

    Added a view to main

--- QtStuff/main.cpp
+++ QtStuff/main.cpp
@@ -11,7 +11,7 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+    QGraphicsView view;
+    view.setFixedSize(600,600);
     return a.exec();
 }
