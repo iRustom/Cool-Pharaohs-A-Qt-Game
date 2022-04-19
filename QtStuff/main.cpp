952745a91b037510f566066dd82f7de18d7ec5cd
OmarElfouly <omarelfouly@aucegypt.edu>
2022-04-20 00:04:13 +0200

    Added libraries

--- QtStuff/main.cpp
+++ QtStuff/main.cpp
@@ -1,5 +1,11 @@
 #include "mainwindow.h"
-
+#include <QApplication>
+#include <QGraphicsScene>
+#include <QGraphicsView>
+#include <QBrush>
+#include <QFile>
+#include <QTextStream>
+#include <QGraphicsPixmapItem>
 #include <QApplication>
 
 int main(int argc, char *argv[])
